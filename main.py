076669b9051b3043bb2608707e856fb362ab3cf4
andersonf007 <andersonfseabra3@gmail.com>
2019-06-27 15:00:15 -0300

    adicionado metodo de carregar dasdos do arquivo

--- main.py
+++ main.py
@@ -3,14 +3,41 @@ patrimonio = []
 professor = []
 acesso = []
 
+def carregarPatrimonios():
+
+	db = open('patrimonio','r')
+	for entrada in db:
+		dados = entrada.split("/")
+		patrimonio.apeend({'nome': dados[0],'numero': dados[1],'disponibilidade': dados[2]})
+	db.close()
+
+	print(patrimonio)
+def carregarProfessor():
+
+	db = open('professor','r')
+	for entrada in db:
+		dados = entrada.split("/")
+		professor.apeend({dados[0],dados[1],dados[2]})
+	db.close()
+
+def carregarAcessos():
+
+	db = open('acesso','r')
+	for entrada in db:
+		dados = entrada.split("/")
+		acesso.apeend({dados[0],dados[1],dados[2],dados[3],dados[4],dados[5]})
+	db.close()
+
 def cadastrarPatrimonio():
+
 	nome = input("Digite o nome do patrimonio:\n")
 	numero = input("Digite o numero do patrimonio:\n")
 	disponibilidade = 0 #onde a disponibilidade for zero o equipamento estará disponivel
 
-	db = open('patrimonio', 'a')
+	db = open('patrimonio','a')
 	db.write('{} / {} / {}\n'.format(nome,numero,disponibilidade))
 	db.close()
+	menu()
 
 def cadastrarProfessor():
 	nome = input("Digite o nome do professor:\n")
@@ -20,6 +47,7 @@ def cadastrarProfessor():
 	db = open('professor', 'a')
 	db.write('{} / {} / {}\n'.format(nome,senha,matricula))
 	db.close()
+	menu()
 
 def menu():
 	print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
@@ -32,7 +60,7 @@ def menu():
 	print("| 6 - Pratimonios disponiveis                                    |")
 	print("| 7 - Listagem dos professores que ficaram com os patrimonios    |")
 	print("| 8 - Listagem dos pratimonios mais utilizados                   |")
-	print("| 0 - Sair 														|")
+	print("| 0 - Sair                                                       |")
 	print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
 	opc = int(input())
@@ -60,6 +88,9 @@ def menu():
 		menu()
 
 
+#carregarAcessos()
+#carregarProfessor()
+carregarPatrimonios()
 #cadastrarPatrimonio()
 #cadastrarProfessor()
 menu()
